6df22877f8978d00b44845d91ddb682c96bb8e44
Brad Gibbons <bradgibbonsdev@gmail.com>
2023-03-03 17:24:03 +0000

    building dark mode further

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function toggleLights() {
     const lightToggle = document.getElementById('body');
+    const logoToggle = document.getElementById('bg-logo');
+    logoToggle.classList.toggle('dark-mode-logo')
     lightToggle.classList.toggle('dark-mode')
     console.log('test')
 }
