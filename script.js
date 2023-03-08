56f368235c5817aa7e6824f0cf1f95d9968070b8
Brad Gibbons <bradgibbonsdev@gmail.com>
2023-03-08 16:23:48 +0000

    finishing dark mode with svg

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
 function toggleLights() {
     const lightToggle = document.getElementById('body');
-    const logoToggle = document.getElementById('bg-logo');
-    logoToggle.classList.toggle('dark-mode-logo')
+    // const logoToggle = document.getElementById('bg-logo');
+    // const cogToggle = document.getElementsByClassName('Path-4');
+    const bToggle = document.getElementById('Combined-Shape');
+    const gbitToggle = document.getElementById('Path-5');
+    const gToggle = document.getElementById('Group-3');
+    const cogToggle = document.querySelectorAll("[id='Path-4']");
+    const bradGibbonsToggle = document.getElementById('brad-gibbons-digital')
+ 
+    for(var i = 0; i < cogToggle.length; i++) 
+    cogToggle[i].classList.toggle('dark-mode-logo'); // <-- whatever you need to do here.
+    bradGibbonsToggle.classList.toggle('dark-mode-logo')
     lightToggle.classList.toggle('dark-mode')
+    gToggle.classList.toggle('dark-mode-logo')
+    bToggle.classList.toggle('dark-mode-logo')
+    gbitToggle.classList.toggle('dark-mode-logo')
     // console.log('test')
 
     // Add animation to div background 
